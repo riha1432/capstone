80be5a8a213e99e30f9b68aa0733f61e794be86c
tjqja <tjqjaejr9741@naver.com>
2024-04-21 13:33:34 +0900

    test

--- python test/cam.py
+++ python test/cam.py
@@ -1,19 +1,41 @@
 import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import time
 
-cap = cv2.VideoCapture(1)
-if cap.isOpened():
-	print('width: {}, height : {}'.format(cap.get(3), cap.get(4)))
-
+# read images
+capture = cv2.VideoCapture(0)
+capture.set(3, 1280)
+capture.set(4, 480)
+s = time.time()
 while True:
-	ret, fram = cap.read()
-	
-	if ret:
-		cv2.imshow('video', fram)
-		k = cv2.waitKey(1) & 0xFF
-		if k == 27:
-			break
-	else:
-		print('error')
+    ret, frame = capture.read()
+    # imgL = cv2.cvtColor(frame[:, :640], cv2.COLOR_BGR2GRAY)
+    # cv2.imshow("L",imgL)
 
-cap.release()
-cv2.destroyAllWindows()	
+    # # 오른쪽 이미지qqq
+    # imgR = cv2.cvtColor(frame[:, 640:], cv2.COLOR_BGR2GRAY)
+    # cv2.imshow("R",imgR)
+    # estimate depth
+    # stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+    # disparity = stereo.compute(imgL, imgR)
+
+    stereo = cv2.StereoSGBM.create(numDisparities=96, blockSize=1)
+    disparity = stereo.compute(frame[:,:640], frame[:,640:])
+    cv2.imshow("R",frame[:,:640])
+	
+    if time.time() - s > 0.5 :
+        s = time.time()
+        print(np.average(disparity[:,:]), end='   /    ')
+        print(np.average(disparity[200:280,280:360]))
+        
+    # cv2.imshow("a", disparity)
+    cv2.waitKey(20)
+
+
+    # stereo = cv2.StereoBM_create(numDisparities=96, blockSize=9)
+    # disparity = stereo.compute(imgL, imgR)
+    plt.figure(figsize=(7,7))
+    plt.imshow(disparity)
+    plt.colorbar()
+    plt.show()
