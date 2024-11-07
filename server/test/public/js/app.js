f67d6ae55c48d72e21a7a08fcc3a58044b53dd50
riha1432 <tjqjaejr9741@gmail.com>
2024-11-07 10:42:21 +0900

    파일 정리 및 분리

--- server/test/public/js/app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const socket = io(); // 서버에 접속
-    const startBtn = document.getElementById("startBtn");
-
-    startBtn.addEventListener("click", () => {
-		console.log("click")
-        socket.emit("data", "start");
-    });
-});
